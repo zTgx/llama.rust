ae5bb253984093944396adf81cd7bf194d41fcc7
zTgx <747674262@qq.com>
2025-08-10 23:55:47 +0800

    define sigmoid

--- sigmoid/src/lib.rs
+++ sigmoid/src/lib.rs
@@ -1,9 +1,15 @@
 use {
-    candle_core::{Tensor, Result}, candle_nn::Module,
+    candle_core::{Tensor, Result}, candle_nn::{ops::sigmoid, Module},
 };
 
 pub struct Sigmoid {
     pub params: Vec<Tensor>,
     pub grads: Vec<Tensor>,
     pub out: Option<Tensor>,
+}
+
+impl Module for Sigmoid {
+    fn forward(&self, xs: &Tensor) -> Result<Tensor> {
+        sigmoid(xs)
+    }
 }
